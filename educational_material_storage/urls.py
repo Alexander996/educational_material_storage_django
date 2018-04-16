63042ad01468f3a84bf1b279db551e6f8902de6a
Alexander Smirnov <al.smirnov996@gmail.com>
2018-04-16 16:30:45 +0300

    Fix static files

--- educational_material_storage/urls.py
+++ educational_material_storage/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
+from educational_material_storage import settings
 from users.auth_token import AuthToken
 
 urlpatterns = [
@@ -24,3 +27,8 @@ urlpatterns = [
     path('api/users/', include('users.urls')),
     path('api/', include('books.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns() + static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
